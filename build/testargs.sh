515f29270b3764bf0893f219aa5ac582e654c0c1
Arjun Roy Chaudhuri <arroyc@microsoft.com>
2020-05-07 01:43:07 -0700

    try 1

---
+++ build/testargs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+# --------------------------------------------------------------------------------------------
+
+set -ex
+
+declare -r REPO_DIR=$( cd $( dirname "$0" ) && cd .. && pwd )
+
+# Load all variables
+source $REPO_DIR/build/__variables.sh
+source $REPO_DIR/build/__functions.sh
+source $REPO_DIR/build/__sdkStorageConstants.sh
+
+firstArg="$1"
+secondArg="$2"
+thirdArg="$3"
+
+if [[ -n $firstArg ]]; then
+    echo "First Arg is: "$firstArg
+fi
+
+if [[ -n $secondArg ]]; then
+    echo "Second Arg is: "$secondArg
+fi
+
+if [[ -n $thirdArg ]]; then
+    echo "Third Arg is: "$thirdArg
+fi
